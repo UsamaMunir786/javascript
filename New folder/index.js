b6899a6eea4e7a76691a242a372baa3867ff7f36
usama <70121208@student.uol.edu.pk>
2024-01-30 21:02:07 +0500

    hello

---
+++ New folder/index.js
@@ -0,0 +1,18 @@
+var plan1 = {
+    name: "Basic",
+    price: 3.99,
+    space: 100,
+    transfer: 1000,
+    pages: 10,
+    paid: false,
+    cart: ["apple", "banana"],
+    sayHello: function(){
+        alert("Hello");
+    }
+};
+
+console.log(plan1);
+// delete plan1.cart;
+// delete plan1.transfer;
+// console.log(plan1);
+// plan1.sayHello();
